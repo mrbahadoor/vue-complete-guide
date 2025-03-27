c32522fdcd517705580d7637738200e239c7c410
Rushdi Bahadoor <my_name@example.com>
2025-03-27 22:23:00 +0400

    v-on directive

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            courseGoal: 'Interpolation',
-            courseGoalA: 'Finish the course and learn vue!',
-            courseGoalB: '<u>Master vue and build amazing apps!</u>',
-            vueLink: 'https://vuejs.org'
-        }
-    },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
-        }
-    }
+  data() {
+    return {
+      counter: 0,
+    };
+  },
 });
-app.mount('#user-goal');
+
+app.mount('#events');
