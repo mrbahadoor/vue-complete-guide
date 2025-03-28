cd2eed11a21985e0b4bc372a5b874594b1a22351
Rushdi Bahadoor <my_name@example.com>
2025-03-28 20:46:42 +0400

    Native event object

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
+    setName(event, lastname) { //event is a vanilla JS default parameter if code is called without parentheses
+      this.name = event.target.value + ' ' + lastname ;
+    },
     add(num) {
       this.counter += num;
     },
