45e09a7e9511f8c2313ff29adda5bee4336c65a2
Rushdi Bahadoor <my_name@example.com>
2025-03-29 17:54:41 +0400

    2 way binding with v-model

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
-      confirmedName: ''
     };
   },
   methods: {
-    setName(event, lastname) { //event is a vanilla JS default parameter if code is called without parentheses
-      this.name = event.target.value + ' ' + lastname ;
-    },
     add(num) {
       this.counter += num;
     },
     reduce(num) {
       this.counter -= num;
     },
-    submitForm() {
-      alert('submitted');
-    },
-    confirmInput() {
-      this.confirmedName = this.name;
+    resetInput() {
+      this.name = '';
     }
   }
 });
